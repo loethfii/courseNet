1ccc9aeaac3842d841741b4bd115f75e0bacee59
Luthfi <luthfiroma00@gmail.com>
2023-05-16 22:06:49 +0700

    pertemuan 4 update1

--- hari-4/method/method.go
+++ hari-4/method/method.go
@@ -3,7 +3,12 @@ package main
 import (
 	"errors"
 	"fmt"
+	"reflect"
+	"strconv"
 )
+
+type cetak interface {
+}
 
 func test(test int) {
 	fmt.Println("test : ", test)
@@ -28,4 +33,53 @@ func main() {
 	} else {
 		fmt.Println(aman)
 	}
+
+	fmt.Println()
+	fmt.Println(jalanJalan("leleleel"))
+
+	hasil := jumlah(7, 7)
+
+	fmt.Println(hasil)
+
+	nilai := nilaiSiswa("Ahmad", 90)
+
+	fmt.Println(nilai)
+
+	var angkaHari uint32 = 29000
+
+	intToString := strconv.Itoa(int(angkaHari))
+
+	fmt.Println(angkaHari)
+
+	fmt.Println(reflect.TypeOf(intToString))
+
+	hKurang, hBagi := kurangDanBagi(100, 10)
+
+	fmt.Println(hKurang)
+	fmt.Println(hBagi)
+
+}
+
+func kurangDanBagi(angka1, angka2 uint32) (uint32, uint32) {
+	kr := angka1 - angka2
+	bg := angka1 / angka2
+	return kr, bg
+}
+
+func nilaiSiswa(nama string, nilai uint32) (resp string) {
+	stringNilai := strconv.FormatUint(uint64(nilai), 10)
+	resp = "Nama : " + nama + " " + stringNilai
+
+	return
+}
+
+func jumlah(angka1 int, angka2 int) int {
+	var hasil = angka1 + angka2
+	return hasil
+}
+
+func jalanJalan(test string) (resp string) {
+	resp = "jalan jalan" + "di Malaka 4"
+
+	return
 }
